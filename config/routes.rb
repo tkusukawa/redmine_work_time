88b2e2e5a36744a5f92c17c57f14b02f766ba33b
kusu <devnull@localhost>
2012-06-10 23:57:18 +0900

    fixes #1050 Redmine2.0への対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'work_time', :controller => 'work_time', :action => :index
-  map.connect 'work_time/:action/:id', :controller => 'work_time', :action => :show
-  map.connect 'work_time', :controller => 'work_time', :action => :total
-  map.connect 'work_time', :controller => 'work_time', :action => :total_data
-  map.connect 'work_time', :controller => 'work_time', :action => :edit_relay
-  map.connect 'work_time', :controller => 'work_time', :action => :relay_total
-  map.connect 'work_time', :controller => 'work_time', :action => :relay_total_data
-  map.connect 'work_time', :controller => 'work_time', :action => :popup_select_ticket
-  map.connect 'work_time', :controller => 'work_time', :action => :popup_select_tickets
-  map.connect 'work_time', :controller => 'work_time', :action => :popup_update_done_ratio
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_select_ticket
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_select_tickets
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_update_done_ratio
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_insert_daily
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_memo_edit
-  map.connect 'work_time', :controller => 'work_time', :action => :ajax_relay_table
+RedmineApp::Application.routes.draw do
+  match 'work_time/:action', :to => 'work_time#index'
+  match 'work_time/:action/:id', :to => 'work_time#show'
 end
--- init.rb
+++ init.rb
@@ -4,15 +4,20 @@ Redmine::Plugin.register :redmine_work_time do
   name 'Redmine Work Time plugin'
   author 'Tomohisa Kusukawa'
   description 'A plugin to view and update TimeEntry by each user'
-  version '0.1.7'
+  version '0.2.0'
   
   project_module :work_time do
     permission :view_work_time_tab, {:work_time =>
-            [:show,:total,:total_data,:edit_relay,:relay_total,:relay_total_data,:popup_select_ticket,:ajax_select_ticket,:popup_select_tickets,:ajax_select_tickets,:ajax_insert_daily,:ajax_memo_edit,:ajax_relay_table]}
+            [:show,:total,:total_data,:edit_relay,:relay_total,
+            :relay_total_data,:popup_select_ticket,:ajax_select_ticket,
+            :popup_select_tickets,:ajax_select_tickets,:ajax_insert_daily,
+            :ajax_memo_edit,:ajax_relay_table]}
     permission :view_work_time_other_member, {}
     permission :edit_work_time_total, {}
   end
 
-  menu :account_menu, :work_time, {:controller => 'work_time', :action => 'index'}, :caption => :work_time
-  menu :project_menu, :work_time, {:controller => 'work_time', :action => 'show'}, :caption => :work_time
+  menu :account_menu, :work_time,
+    {:controller => 'work_time', :action => 'index'}, :caption => :work_time
+  menu :project_menu, :work_time,
+    {:controller => 'work_time', :action => 'show'}, :caption => :work_time
 end
