b2340ffadf010e0070f56e70a5b56d88447873ab
kusu <devnull@localhost>
2012-09-23 14:22:49 +0900

    【リファクタリング】 erbに散らばっていたJSをwork_time.jsに収集 AタグのJS表記を統一

--- assets/javascripts/work_time.js
+++ assets/javascripts/work_time.js
@@ -63,5 +63,124 @@ function update_done_ratio(pop_url, rep_url, issue_id)
       drs[i].innerHTML = "["+done_ratio+"&#37;]";
     }
   }
+}
 
+function del_ticket_relay(rep_url, child)
+{
+    jQuery.ajax({
+        url:rep_url+"&ticket_relay="+child+"_0",
+        data:{asynchronous:true, method:'get'
+      },
+      success:function(response){
+        jQuery('#ticket'+child).html(response);
+      }
+    });
+}
+
+function checkKey(e, finish_func)
+{
+  if (!e) var e = window.event;
+  if(e.keyCode == 13) {
+    finish_func();
+    return false;
+  }
+  else
+    return e;
+}
+
+function ajax_select_tickets(rep_url)
+{
+    jQuery.ajax({
+      url:rep_url,
+      data:{asynchronous:true, method:'get'},
+      success:function(response){
+        jQuery('#tickets').html(response);
+      }
+    });
+}
+
+//------------------------------------------------- for show.html.erb
+var add_ticket_count = 1;
+function add_ticket(pop_url, ajax_url)
+{
+    var tickets = showModalDialog(pop_url, window, "dialogWidth:600px;dialogHeight:480px");
+    for(i=0; i<tickets.length;i++) {
+        jQuery.ajax({
+          url:ajax_url+"&add_issue="+tickets[i]+"&count="+add_ticket_count,
+          data:{asynchronous:true, method:'get'},
+          success:function(response){
+            jQuery('#time_input_table_bottom').before(response);
+          }
+       });
+      add_ticket_count ++;
+    }
+}
+
+function dup_ticket(ajax_url, insert_pos, id)
+{
+    jQuery.ajax({
+      url:ajax_url+"&add_issue="+id+"&count="+add_ticket_count,
+      data:{asynchronous:true, method:'get'},
+      success:function(response){
+        jQuery('#'+insert_pos).after(response);
+      }
+    });
+  add_ticket_count ++;
+}
+
+function edit_memo(ajax_url)
+{
+    jQuery.ajax({
+      url:ajax_url,
+      data:{asynchronous:true, method:'get'},
+      success: function(response){
+        jQuery('#memo-wiki').html(response);
+      }
+    });
+}
+
+//---------------------------------------- for popup_update_done_ratio.html.erb
+function ratio_inputed()
+{
+  returnValue = document.getElementById("input_ratio").value;
+  close();
+}
+
+//--------------- for popup_select_ticket.html.erb, ajax_select_ticket.html.erb
+function ticket_inputed()
+{
+  returnValue = document.getElementById("input_id").value;
+  close();
+}
+
+function ticket_selected(issue_id)
+{
+  returnValue = issue_id;
+  close();
+}
+
+//------------- for popup_select_tickets.html.erb, ajax_select_tickets.html.erb
+function tickets_inputed()
+{
+  var vals = document.getElementById("input_ids").value;
+  var tickets = vals.split(',');
+  returnValue=tickets;
+  close();
+}
+
+function tickets_selected(issue_id)
+{
+  returnValue = [issue_id];
+  close();
+}
+
+function tickets_checked()
+{
+  var issue_ids = new Array;
+  for(i=0; e = document.forms[0].elements[i]; i++) {
+    if(e.checked)
+      issue_ids.push(e.value);
+  }
+  returnValue = issue_ids;
+  close();
 }
