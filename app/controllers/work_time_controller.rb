bc8e7425a78110c67c565fdf8ec3447eb0f19dfb
Tomohisa Kusukawa <t.kusukawa@gmail.com>
2014-06-29 22:59:48 +0900

    with ticket number

--- app/controllers/work_time_controller.rb
+++ app/controllers/work_time_controller.rb
@@ -66,7 +66,7 @@ class WorkTimeController < ApplicationController
           next if issue_pack[:count_hours] == 0
           issue = issue_pack[:issue]
           if issue_pack[:total_by_day][date] then
-            csv_data << %Q|"#{@this_user}","#{date}","#{issue.project}","#{issue.subject}",#{issue_pack[:total_by_day][date]}\n|
+            csv_data << %Q|"#{@this_user}","#{date}","#{issue.project}","##{issue.id} #{issue.subject}",#{issue_pack[:total_by_day][date]}\n|
           end
         end
       end
@@ -134,8 +134,8 @@ class WorkTimeController < ApplicationController
           parent_issue = Issue.find_by_id(@issue_parent[issue_id])
           next if parent_issue.nil? # チケットが削除されていたらパス
 
-          csv_data << %Q|"#{user}","#{parent_issue.project}","#{parent_issue.subject}",|
-          csv_data << %Q|"#{prj}","#{issue.to_s}",#{@issue_cost[issue_id][user.id]}\n|
+          csv_data << %Q|"#{user}","#{parent_issue.project}","##{parent_issue.id} #{parent_issue.subject}",|
+          csv_data << %Q|"#{prj}","##{issue.id} #{issue.subject}",#{@issue_cost[issue_id][user.id]}\n|
         end
       end
       if @issue_cost.has_key?(-1) && @issue_cost[-1].has_key?(user.id) then
@@ -213,7 +213,7 @@ class WorkTimeController < ApplicationController
           next if issue.nil? # チケットが削除されていたらパス
           next if issue.project_id != dsp_prj # このプロジェクトに表示するチケットでない場合はパス
 
-          csv_data << %Q|"#{user}","#{prj}","#{issue.id} #{issue.subject}",#{@r_issue_cost[issue_id][user.id]}\n|
+          csv_data << %Q|"#{user}","#{prj}","##{issue.id} #{issue.subject}",#{@r_issue_cost[issue_id][user.id]}\n|
         end
       end
       if @r_issue_cost.has_key?(-1) && @r_issue_cost[-1].has_key?(user.id) then
