f9f8f86d39dd1385779614e0bc106ee6c423912d
tkusukawa <tkusukawa@olbia.local>
2017-03-06 00:37:14 +0900

    Delete database index conflicting with Redmine 3.4

--- db/migrate/20141029173025_add_parent_id_index_to_issues.rb
+++
@@ -1,9 +0,0 @@
-class AddParentIdIndexToIssues < ActiveRecord::Migration
-  def self.up
-    add_index :issues, :parent_id
-  end
-
-  def self.down
-    remove_index :issues, :column => :parent_id
-  end
-end
